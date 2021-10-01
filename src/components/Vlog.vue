1a0531fcbc1660d657d9c24365538807abe45e6d
BigFelk <fedferrara89@gmail.com>
2021-10-01 12:00:34 +0200

    blog finito 10 componenti rimasti

--- src/components/Vlog.vue
+++ src/components/Vlog.vue
@@ -1,7 +1,31 @@
 <template>
-  <div class="container">
-      prova
-  </div>
+    <div class="container">
+        <h2>From Our Blog</h2>
+        <div class="centro">The latest Classic Shop news</div>
+        <div class="prova">
+            <div class="colonnina">
+               <img  class="resize" src="@/assets/post_img_10-300x188.jpg" alt="">
+               <div class="mt"><strong> la ragazza nella natura</strong></div>
+               <div class="mb">lorem ipsumm</div>
+               <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus et nam placeat suscipit magnam nemo nihil consequatur quasi aperiam. Officiis itaque consequuntur provident esse ea in est ipsum porro ipsa!</div>
+            </div>
+            <div class="colonnina">
+                <img class="resize" src="@/assets/post_img_11-300x188.jpg" alt="">
+                <div><strong> la ragazza nella natura</strong></div>
+                <div>lorem ipsumm</div>
+                <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus et nam placeat suscipit magnam nemo nihil consequatur quasi aperiam. Officiis itaque consequuntur provident esse ea in est ipsum porro ipsa!</div>
+            
+            </div>
+            <div class="colonnina">
+                <img class="resize" src="@/assets/post_img_12-300x188.jpg" alt="">
+                <div><strong> la ragazza nella natura</strong></div>
+                <div>lorem ipsumm</div>
+                <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus et nam placeat suscipit magnam nemo nihil consequatur quasi aperiam. Officiis itaque consequuntur provident esse ea in est ipsum porro ipsa!</div>
+            
+            </div>
+        </div>
+         
+    </div>
 </template>
 
 <script>
@@ -11,10 +35,59 @@ export default {
 </script>
 
 <style scoped  lang="scss">
+.mt{
+    margin-top: 5px;
+}
+.mb{
+    margin-bottom: 5px;
+}
+.resize{
+    width: 100%;
+}
+ 
+.prova{
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    margin: 0;
+}
+.colonnina{
+     
+    width: calc(100% / 3 - 10px);
+     
+}
     .container{
         width: 80% ;
-        height: 200px;
-        background-color: aquamarine;
+        height: 500px;
         margin: 0 auto;
     }
+  h2 {
+  overflow: hidden;
+  text-align: center;
+}
+
+h2:before,
+h2:after {
+  background-color:  #d1d1d1;
+  content: "";
+  display: inline-block;
+  height: 1px;
+  position: relative;
+  vertical-align: middle;
+  width: 50%;
+}
+
+h2:before {
+  right: 20px;
+  margin-left: -50%;
+}
+
+h2:after {
+  left: 20px;
+  margin-right: -50%;
+}
+.centro{
+    text-align: center;
+    margin-bottom: 30px;
+}
 </style>

0e2c1990df93442ed5f06da9d79642258a62af36
BigFelk <fedferrara89@gmail.com>
2021-10-01 14:56:32 +0200

    testa finita 8 al termine

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,46 @@
 <template>
-  <div class="container">
-      prova
-  </div>
+    <div class="container">
+        <div class="sopra">
+          <div class="margi80">
+                <div class="iconcine">
+                    <i class="fab fa-facebook-f"></i>
+                    <i class="fab fa-twitter"></i>
+                    <i class="fab fa-instagram"></i>
+                    <i class="fab fa-youtube"></i>
+                </div>
+                <div class="spesa">
+                    <div class="shopping">
+                        Shopping cart
+                    </div>
+                    <div class="account">
+                        <div>
+                            My account <i class="fas fa-angle-down ml"></i>
+                        </div>
+                    </div>
+                    <div class="cart">
+                         <i class="fas fa-shopping-cart"></i> CART <i class="fas fa-angle-down ml"></i>
+                    </div>
+                </div>
+          </div>
+            
+        </div>
+        <div class="sotto">
+            <div class="immagi">
+               <img   src="@/assets/classic_shop_logo1x.png" alt="">
+            </div>
+            <div class="navigatore">
+                <div>Home<i class="fas fa-angle-down ml"></i></div>
+                <div>Shop<i class="fas fa-angle-down ml"></i></div>
+                <div>Producs<i class="fas fa-angle-down ml"></i></div>
+                <div>Categories<i class="fas fa-angle-down ml"></i></div>
+                <div>News</div>
+                <div>Elements<i class="fas fa-angle-down ml"></i></div>
+                <div class="pulsante">SHOP NOW!</div>
+                <div><i class="fas fa-search"></i></div>
+            </div>
+        </div>
+    
+    </div>
 </template>
 
 <script>
@@ -11,10 +50,83 @@ export default {
 </script>
 
 <style scoped  lang="scss">
-    .container{
-        width: 80% ;
-        height: 200px;
-        background-color: aquamarine;
+    .pulsante{
+        background-color: #427ED1;
+        padding: 7px 20px;
+        color: white;
+        border-radius: 20px;
+        margin: 10px;
+    }
+    .immagi{
+        width: 25%;
+         height: 140px;
+        line-height: 140px;
+
+    }
+    .navigatore{
+        width: 65%;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    .sotto{
+        width: 80%;
         margin: 0 auto;
+        display: flex;
+        justify-content: space-between;
+    }
+    .ml{
+        margin-left: 5px;
+    }
+    .shopping{
+        width: 25%;
+    }
+    .account{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 25%;
+        border: 1px solid #d1d1d1;
+        height: 70px;
+    }
+    .cart{
+        width: 50%;
+        height: 70px;
+        line-height: 70px;
+        text-align: center;
+        background-color: #FAFAFA;
+    }
+    .iconcine{
+        width: 15%;
+        justify-content: space-between;
+        display: flex;
+        align-items: center;
+    }
+    .spesa{
+        display: flex;
+        align-items: center;
+        width: 40%;
+        height: 70px;
+         
+    }
+    .margi80{
+        
+        display: flex;
+        justify-content: space-between;
+        width: 80%;
+        margin: 0 auto;
+    }
+    .container{
+        width: 100% ;
+        height: 210px;
+        
+        margin: 0 auto;
+    }
+    .sopra{
+        height: 70px;
+        width: 100%;
+       
+        border-bottom:1px solid #d1d1d1;
     }
 </style>

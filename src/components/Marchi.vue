502c12d0ed7c23b3f240f671f1c7419ae2cd6258
BigFelk <fedferrara89@gmail.com>
2021-10-01 10:19:35 +0200

    brand finito

--- src/components/Marchi.vue
+++ src/components/Marchi.vue
@@ -1,7 +1,18 @@
 <template>
-  <div class="container">
-      marchi
-  </div>
+ <div class="sfondo">
+    <div class="container2">
+        <div class="centro">
+            BRAND LOGOS
+        </div>
+        <div class="flex">
+            <img src="@/assets/b_logotype_6.png" alt="">
+            <img src="@/assets/b_logotype_7.png" alt="">
+            <img src="@/assets/b_logotype_1.png" alt="">
+            <img src="@/assets/b_logotype_2.png" alt="">
+            <img src="@/assets/b_logotype_3.png" alt="">
+        </div>
+    </div>
+ </div>
 </template>
 
 <script>
@@ -11,10 +22,22 @@ export default {
 </script>
 
 <style scoped  lang="scss">
-    .container{
+    .sfondo{
+        background-color: #F6F6F6;
+    }
+    .container2{
         width: 80% ;
         height: 200px;
-        background-color: aquamarine;
+        padding-top: 50px;
         margin: 0 auto;
     }
+    .centro{
+        text-align: center;
+        padding-bottom: 20px;
+    }
+    .flex{
+        display: flex;
+        justify-content: space-between;
+    }
+
 </style>
